fd75ffe2bcba3800e75cc663fa54f2e093c4fb2a
lucvanhau <haucdt@gmail.com>
2024-09-03 23:14:31 +0700

    Add files via upload

---
+++ daoquan/src/chap.js
@@ -0,0 +1,20 @@
+function execute(url) {
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html();
+        let htm = doc.select("div.box-chap").html();
+        let fix = newfix(htm);
+        return Response.success(htm);
+    }
+    return null;
+}
+function newfix(text)
+{
+if(text){
+let aa = text.replace(/\.\n/g, 'aaaaa');
+let bb = aa.replace(/<br>/g, ' ');
+    let cc = bb.replace(/aaaaa/g,'.<br>');
+    return cc;
+}
+   return "lỗi gì đó"; 
+}
